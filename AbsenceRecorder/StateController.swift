bda04e25452f5ce773302e2937d98ee594840ca9
Sritej Tummuru <sritej.tummuru@gmail.com>
2023-02-23 22:51:24 +0000

    Added persistence of user data using JSON files that are locally stored on the user's device.

--- AbsenceRecorder/StateController.swift
+++ AbsenceRecorder/StateController.swift
@@ -8,10 +8,46 @@
 import Foundation
 
 class StateController: ObservableObject {
-    @Published var divisions: [Division]
+    @Published var divisions: [Division] = []
     @Published var totalStudents = 46
     
     init() {
-        divisions = Division.examples
+        loadFromFile()
+    }
+    
+    func getFilePath(fileName: String) -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let userPath = paths[0]
+        let url = userPath.appendingPathComponent(fileName)
+        return url
+    }
+    
+    func loadFromFile() {
+        let url = getFilePath(fileName: "divisions.json")
+        if let data = try? Data(contentsOf: url) {
+            let decoder = JSONDecoder()
+            if let loaded = try? decoder.decode([Division].self, from: data) {
+                divisions = loaded
+            }
+        }
+        
+    }
+    
+    func saveToFile() {
+        // create json encoder
+        let encoder = JSONEncoder()
+        if let encoded =  try? encoder.encode(divisions) {
+            // take encoded objects and convert into string
+            if let json = String(data: encoded, encoding: .utf8) {
+                // create file path
+                let url = getFilePath(fileName: "divisions.json")
+                // write json string to file
+                do {
+                    try json.write(to: url, atomically: true, encoding: .utf8)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
